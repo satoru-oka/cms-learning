16346966173c451c797ca012ed995f0757d51d9d
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-14 14:23:18 +0900

    fixed sidebar

--- index.php
+++ index.php
@@ -25,51 +25,31 @@
 
                 ?>
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#"><?= $post_title ?></a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php"><?= $post_author ?></a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
-                <hr>
-                <img class="img-responsive" src="images/<?= $post_image ?>" alt="">
-                <hr>
-                <p><?= $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More<span class="glyphicon glyphicon-chevron-right"></span></a>
-                
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?= $post_title ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php"><?= $post_author ?></a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
+                    <hr>
+                    <img class="img-responsive" src="images/<?= $post_image ?>" alt="">
+                    <hr>
+                    <p><?= $post_content ?></p>
+                    <a class="btn btn-primary" href="#">Read More<span class="glyphicon glyphicon-chevron-right"></span></a>
+                    
                 <?php } ?>
                 
                 <hr>
             </div>
-            <!-- Blog Sidebar Widgets Column -->
 
             <?php require_once('includes/sidebar.php') ?>
-
-            <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Blog Search</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
-                <!-- Blog Categories Well -->
-
 
         </div>
         <!-- /.row -->
