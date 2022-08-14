26502bb578064aa3a24dcc4e8072bfda809e504f
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-14 15:38:27 +0900

    creating a custom serach engine-2

---
+++ search.php
@@ -0,0 +1,73 @@
+<?php require_once('includes/db.php') ?>
+<?php require_once('includes/header.php') ?>
+    <!-- Navigation -->
+    <?php require_once "includes/navigation.php" ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+            <?php
+                if (isset($_POST['submit'])) {
+
+                    $search = $_POST['search'];
+                    
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%' "; 
+                    $search_query = mysqli_query($connection, $query);
+                    if (!$search_query) {
+                        die("Query FAILED" . mysqli_error($connection));
+                    }
+
+                    $count = mysqli_num_rows($search_query);
+                    if ($count == 0) {
+                        
+                        echo "<h1> No Result</h1>";
+                    
+                    } else {
+
+                        while ($row = mysqli_fetch_assoc($search_query)) {
+                        
+                            $post_title = $row['post_title'];
+                            $post_author = $row['post_author'];
+                            $post_date = $row['post_date'];
+                            $post_image = $row['post_image'];
+                            $post_content = $row['post_content'];
+                        
+                        }
+            ?>
+                        <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                        <a href="#"><?= $post_title ?></a>
+                        </h2>
+                        <p class="lead">
+                        by <a href="index.php"><?= $post_author ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?= $post_image ?>" alt="">
+                        <hr>
+                        <p><?= $post_content ?></p>
+                        <a class="btn btn-primary" href="#">Read More<span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <hr>
+                <?php } ?>
+            <?php  }  ?>
+
+
+                
+            </div>
+
+            <?php require_once('includes/sidebar.php') ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+<?php require_once('includes/footer.php') ?>
