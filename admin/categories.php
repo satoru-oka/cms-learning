921e74658a9c693a55d24b5c8b69ce15a3ee8a36
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-14 17:49:24 +0900

    creating category page 2

--- admin/categories.php
+++ admin/categories.php
@@ -17,6 +17,35 @@
                             <small>Admin</small>
                         </h1>
 
+                        <div class="col-xs-6">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="cat-title">Add Category</label>
+                                    <input type="text" class="form-control" name="cat_title">
+                                </div>
+                                <div class="form-group">
+                                    <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                                </div>
+                            </form>
+                        </div><!-- Add Category Form -->
+
+                        <div class="col-xs-6">
+                            <table class="table table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Category Title</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Baseball Category</td>
+                                        <td>Basketball Category</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
                 <!-- /.row -->
