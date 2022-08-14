d85fc07ccd635782deb1a33e941f27ae6b7217f4
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-14 12:57:03 +0900

    create posts table

--- index.php
+++ index.php
@@ -10,6 +10,21 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+                <?php 
+                
+                    $query = "SELECT * FROM posts";
+                    $select_all_posts_query = mysqli_query($connection, $query);
+
+                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                        
+                        $post_title = $row['post_title'];
+                        $post_author = $row['post_author'];
+                        $post_date = $row['post_date'];
+                        $post_imgae = $row['post_image'];
+                        $post_content = $row['post_content'];
+
+                ?>
+
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -17,20 +32,22 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?= $post_title ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?= $post_author ?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span><?= $post_date ?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
+                <p><?= $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More<span class="glyphicon glyphicon-chevron-right"></span></a>
+                
+                <?php } ?>
+                
                 <hr>
-
+            </div>
             <!-- Blog Sidebar Widgets Column -->
 
             <?php require_once('includes/sidebar.php') ?>
