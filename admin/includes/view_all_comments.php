7f7b7e484f9b9a2a1611b41ecff2c55e6106ded2
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-16 17:08:40 +0900

    creating the query for displaying comments in admin part2

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -17,12 +17,12 @@
         <tbody>
 
         <?php
-            $query = "SELECT * FROM posts comments";
+            $query = "SELECT * FROM comments";
             $select_comments = mysqli_query($connection, $query);
         
             while ($row = mysqli_fetch_assoc($select_comments)) {
         
-                $comment__id = $row['comment_id'];
+                $comment_id = $row['comment_id'];
                 $comment_post_id = $row['comment_post_id'];
                 $comment_author = $row['comment_author'];
                 $comment_content = $row['comment_content'];
@@ -34,7 +34,6 @@
                 echo "<td>$comment_id</td>";
                 echo "<td>$comment_author</td>";
                 echo "<td>$comment_content</td>";
-                echo "<td>$comment_email</td>";
                 
                 // $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id} ";
                 // $select_categories_id = mysqli_query($connection, $query);
@@ -45,18 +44,19 @@
 
                 //     $cat_id = $row['cat_id'];
                 //     $cat_title = $row['cat_title'];
-                    
+                
                 //     echo "<td>{$cat_title}</td>";
-                    
+                
                 // }
                 
                 
+                echo "<td>$comment_email</td>";
                 echo "<td>$comment_status</td>";
+                echo "<td>Some Title</td>";
                 echo "<td>$comment_date</td>";
-                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Approve</a></td>";
-                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Unapprove</a></td>";
-                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-                echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+                echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
+                echo "<td><a href='posts.php?source=edit_post&p_id='>Unapprove</a></td>";
+                echo "<td><a href='posts.php?delete='>Delete</a></td>";
                 echo "</tr>";
         
             }
