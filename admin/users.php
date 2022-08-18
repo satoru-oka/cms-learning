32d56c82c59bd410bea4d037b4cf997ea4919ceb
satoru-oka <57833102+satoru-oka@users.noreply.github.com>
2022-08-18 21:05:26 +0900

    creating users pages and links

---
+++ admin/users.php
@@ -0,0 +1,56 @@
+<?php require_once("includes/admin_header.php") ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php require_once("includes/admin_navigation.php") ?>
+ 
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Admin</small>
+                        </h1>
+
+                        <?php
+
+                        if (isset($_GET['source'])) {
+                            $source = $_GET['source'];
+                        } else {
+                            $source = '';
+                        }
+
+                        // Controls page display according to conditions
+                        switch($source) {
+                            
+                            case 'add_post';
+                                include "includes/add_users.php";
+                                break;
+                            case 'edit_post';
+                                include "includes/edit_users.php";
+                                break;
+                            case '200';
+                                echo "Nice 200";
+                                break;
+
+                            default:
+                            
+                            include "includes/view_all_users.php";
+
+                            break;
+
+                        }
+
+                        ?>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+<?php require_once("includes/admin_footer.php") ?>
